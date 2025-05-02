e8b9682e4ec28793b2bfdc48c256360a5564f486
Emre <emre.karaloglu@megacode.com.tr>
2025-05-02 09:35:43 +0300

    bıdıbıdı2

---
+++ EFK.Commons/Infrastructure/Commons.Persistence/Configurations/Packages/PackageConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Commons.Domain.Models.Packages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Commons.Persistence.Configurations.Packages
+{
+    public class PackageConfiguration : IEntityTypeConfiguration<Package>
+    {
+        public void Configure(EntityTypeBuilder<Package> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Name)
+                   .IsRequired()
+                   .HasMaxLength(200);
+
+            builder.HasMany(c => c.Companies)
+                   .WithOne(o => o.Package)
+                   .HasForeignKey(o => o.PackageId)
+                   .OnDelete(DeleteBehavior.Cascade);
+
+        }
+    }
+}
---
+++ EFK.Commons/Infrastructure/Commons.Persistence/Configurations/Packages/PackageMenuConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Commons.Domain.Models.Packages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Commons.Persistence.Configurations.Packages
+{
+    public class PackageMenuConfiguration : IEntityTypeConfiguration<PackageMenu>
+    {
+        public void Configure(EntityTypeBuilder<PackageMenu> builder)
+        {
+            builder.HasKey(pm => pm.Id);
+
+            builder.Property(pm => pm.Id)
+                   .ValueGeneratedNever(); //  Guid'leri biz atayacağız 
+
+            builder.HasOne(pm => pm.Package)
+                   .WithMany(x => x.PackageMenus)
+                   .HasForeignKey(pm => pm.PackageId)
+                   .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(pm => pm.Menu)
+                   .WithMany(x => x.PackageMenus)
+                   .HasForeignKey(pm => pm.MenuId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
