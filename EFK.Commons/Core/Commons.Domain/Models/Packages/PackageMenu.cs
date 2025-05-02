f0d7d901ffb21a510685498a635ee6bf1b610461
Emre <emre.karaloglu@megacode.com.tr>
2025-05-02 09:29:54 +0300

    bıdıbıdı

---
+++ EFK.Commons/Core/Commons.Domain/Models/Packages/PackageMenu.cs
@@ -0,0 +1,13 @@
+﻿using Commons.Domain.Models.Menus;
+
+namespace Commons.Domain.Models.Packages
+{
+    public class PackageMenu: BaseEntity
+    {
+        public Guid PackageId { get; set; }
+        public Package Package { get; set; }
+
+        public Guid MenuId { get; set; }
+        public Menu Menu { get; set; }
+    }
+}
